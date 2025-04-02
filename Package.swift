7fafd878d105f3086f81e69e5b9722f2de0f20cb
Leslie Owens <ldowens@csdcorp.com>
2025-04-02 11:52:47 -0400

    -updated swift version to support higher iOS version

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.2
+// swift-tools-version:5.10
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Snap",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(name: "Snap",
                  targets: ["Snap"]),
